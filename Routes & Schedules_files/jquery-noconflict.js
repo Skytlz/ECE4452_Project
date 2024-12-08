4c56e109b17050579afd6e9d381c262404dc0ec6
Skytlz <skytlzdev@gmail.com>
2024-12-08 15:50:56 -0500

    Routes & Schedules

---
+++ Routes & Schedules_files/jquery-noconflict.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+jQuery.noConflict();
+
+
+}
+/*
+     FILE ARCHIVED ON 04:35:56 Dec 02, 2024 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 20:45:10 Dec 08, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.717
+  exclusion.robots: 0.029
+  exclusion.robots.policy: 0.014
+  esindex: 0.013
+  cdx.remote: 30.837
+  LoadShardBlock: 83.272 (3)
+  PetaboxLoader3.datanode: 86.918 (4)
+  load_resource: 58.941
+  PetaboxLoader3.resolve: 34.436
+*/
